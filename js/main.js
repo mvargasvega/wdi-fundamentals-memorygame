9c08aea577fa63a50913d3b7085b7ae5c3cb638f
mvargasvega <martinvargasvega@gmail.com>
2018-01-14 23:10:27 -0800

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-console.log("Up and running!");
 
 
 
-
-var cards = ["queen","queen","king", "king"
- /* {
+var cards = [
+  {
   rank: "queen",
   suit: "hearts",
   cardImage: "images/queen-of-hearts.png"
@@ -23,7 +21,7 @@ var cards = ["queen","queen","king", "king"
   rank: "king",
   suit: "diamonds",
   cardImage: "images/king-of-diamonds.png"
-  }*/
+  }
 ];
 var cardsInPlay = [];
 
@@ -36,8 +34,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   if (cardsInPlay.length === 2){
       console.log ("Good Job, lets see if they match")
       checkForMatch();
